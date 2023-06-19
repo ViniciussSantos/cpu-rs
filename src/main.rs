785546b2f81cab9e1e36e6210bb1eb20de73d9cf
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-06-19 20:19:36 -0300

    adding RAM and a counter

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,76 @@
 struct CPU {
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    program_counter: usize,
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let pointer = self.program_counter;
+        let op_byte_1 = self.memory[pointer] as u16;
+        let op_byte_2 = self.memory[pointer + 1] as u16;
+        op_byte_1 << 8 | op_byte_2
     }
 
     fn run(&mut self) {
-        let opcode = self.read_opcode();
+        loop {
+            let opcode = self.read_opcode();
+            self.program_counter += 2;
 
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = (opcode & 0x000F) as u8;
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = (opcode & 0x000F) as u8;
 
-        match (c, x, y, d) {
-            (0x8, _, _, 0x4) => {
-                self.add_xy(x, y);
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => {
+                    self.add_xy(x, y);
+                }
+                _ => todo!("opcode {:04x}", opcode),
             }
-            _ => todo!("opcode {:04x}", opcode),
         }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+        let (res, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = res;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
 
 fn main() {
     let mut cpu = CPU {
-        current_operation: 0,
-        registers: [0; 2],
+        registers: [0; 16],
+        memory: [0; 0x1000],
+        program_counter: 0,
     };
 
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let memory = &mut cpu.memory;
+
+    memory[0] = 0x80;
+    memory[1] = 0x14;
+    memory[2] = 0x80;
+    memory[3] = 0x24;
+    memory[4] = 0x80;
+    memory[5] = 0x34;
+
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 15);
-    println!("5+10 = {}", cpu.registers[0]);
+    assert_eq!(cpu.registers[0], 35);
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
